ba12c3ab3bc1f4d3fec4e413aafc123afff3bbfc
arimaghosh <arimaghosh90@gmail.com>
2020-09-24 19:49:14 +0530

    Fourth Commit

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-$(".fa-bars").click(function(){
-  $(".navbar-collapse").css("display","block");
-  $("#close").css("display","block");
-  $("#close").click(function(){
-    $(".navbar-collapse").css("display","none");
-    $("#close").css("display","none");
+$(".closebtn").hide();
+
+$('.modal').on('show.bs.modal', function () {
+  $("body").css('overflow-y', 'hidden');
+  $("html").css('overflow-y', 'hidden');
 });
+$('.modal').on('hide.bs.modal', function () {
+  $("body").css('overflow-y', 'auto');
+  $("html").css('overflow-y', 'auto');
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  setTimeout(function(){
+    $(".closebtn").show();
+  },450);
+  $("body").css('overflow-y', 'hidden');
+  $("html").css('overflow-y', 'hidden');
+  document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  $(".closebtn").hide();
+  $(".closebtn").css("display","none");
+  $("body").css('overflow-y', 'auto');
+  $("html").css('overflow-y', 'auto');
+  document.body.style.backgroundColor = "white";
+}
