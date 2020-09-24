86c4cae8491ea11e888b6b615969461a333959a5
arimaghosh <arimaghosh90@gmail.com>
2020-09-24 21:12:51 +0530

    Sixth Commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(".closebtn").hide();
+$(".display-business").hide();
+$(".display-teach").hide();
+$(".display-shop").hide();
 
 $('.modal').on('show.bs.modal', function () {
   $("body").css('overflow-y', 'hidden');
@@ -27,3 +30,20 @@ function closeNav() {
   $("html").css('overflow-y', 'auto');
   document.body.style.backgroundColor = "white";
 }
+$(".udemy-business").hover(function(){ //mouseenter
+  $(".display-business").show();
+}, function(){ //mouseleave
+  $(".display-business").hide();
+});
+
+$(".udemy-teach").hover(function(){ //mouseenter
+  $(".display-teach").show();
+}, function(){ //mouseleave
+  $(".display-teach").hide();
+});
+
+$(".udemy-shop").hover(function(){ //mouseenter
+  $(".display-shop").show();
+}, function(){ //mouseleave
+  $(".display-shop").hide();
+});
